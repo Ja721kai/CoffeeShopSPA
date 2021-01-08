71c91e502078d6488ca13c05c882c151e45db8b7
Jan Kaiser <jan.kaiser@devs-group.ch>
2021-01-08 18:02:08 +0100

    bind navigation and home hero into app

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <template>
-    <v-app>
-        <v-content transition="slide-x-transition">
-            <router-view></router-view>
-        </v-content>
-    </v-app>
+  <v-app>
+    <app-navigation></app-navigation>
+
+    <v-content transition="slide-x-transition">
+      <router-view></router-view>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
+import AppNavigation from '@/components/AppNavigation';
+
 export default {
-    name: 'App'
+  name: 'App',
+  components: {
+    AppNavigation
+  }
 };
 </script>
 
