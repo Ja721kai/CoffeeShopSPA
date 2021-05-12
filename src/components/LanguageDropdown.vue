10524c47d3261de48506ffca41a4f7d86aa23cd2
Niklas Pelz <pelznik@gmail.com>
2021-05-12 17:32:37 +0200

    added the Language Dropdown
    Signed-off-by: Niklas Pelz <pelznik@gmail.com>

---
+++ src/components/LanguageDropdown.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-select
+        v-model="e1"
+        :items="languages"
+        prepend-icon="mdi-translate"
+    ></v-select>
+</template>
+
+<script>
+export default {
+    name: 'LanguageDropdown',
+    data() {
+        return {
+            e1: 'German',
+            languages: ['German', 'English']
+        };
+    }
+};
+</script>
+
+<style scoped></style>
