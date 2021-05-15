f218f1a74dade006e82523d43ae29069fb9840d6
Jan Kaiser <32633980+Ja721kai@users.noreply.github.com>
2021-05-15 12:48:07 +0200

    Footer auslagern (#15)
    * renamed files for better readability
    
    Signed-off-by: Niklas Pelz <pelznik@gmail.com>
    
    * added raw-loader, couldnt find it before adding the package
    
    Signed-off-by: Niklas Pelz <pelznik@gmail.com>
    
    * added the Language Dropdown
    
    Signed-off-by: Niklas Pelz <pelznik@gmail.com>
    
    * Added the Regenerate Button + the Language Dropdown
    
    Signed-off-by: Niklas Pelz <pelznik@gmail.com>
    
    * changing languages works now
    
    * added some more phrases, regenerate works kinda
    
    * Refractoring
    
    * Footer nach App ausgelagert
    
    Co-authored-by: Niklas Pelz <pelznik@gmail.com>
    Co-authored-by: Jan Kaiser <jan.kaiser@devs-group.ch>

--- src/views/Imprint.vue
+++ src/views/Imprint.vue
@@ -1,27 +1,16 @@
 <template>
     <span>
         <coffee-imprint></coffee-imprint>
-        <page-footer></page-footer>
     </span>
 </template>
 
 <script>
 import CoffeeImprint from '@/components/CoffeeImprint';
-import PageFooter from '@/components/PageFooter';
 
 export default {
     name: 'imprint',
     components: {
-        CoffeeImprint,
-        PageFooter
+        CoffeeImprint
     }
 };
 </script>
-
-<style scoped>
-.page-footer {
-    position: fixed;
-    left: 0;
-    bottom: 0;
-}
-</style>
