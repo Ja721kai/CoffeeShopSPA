d5f7591b3f96a29e8c6943f6913139ba55d81c9e
Jan Kaiser <32633980+Ja721kai@users.noreply.github.com>
2021-05-15 08:37:34 +0200

    Adjust coffee cards (#14)
    * renamed files for better readability
    
    Signed-off-by: Niklas Pelz <pelznik@gmail.com>
    
    * added raw-loader, couldnt find it before adding the package
    
    Signed-off-by: Niklas Pelz <pelznik@gmail.com>
    
    * added the Language Dropdown
    
    Signed-off-by: Niklas Pelz <pelznik@gmail.com>
    
    * Added the Regenerate Button + the Language Dropdown
    
    Signed-off-by: Niklas Pelz <pelznik@gmail.com>
    
    * changing languages works now
    
    * added some more phrases, regenerate works kinda
    
    * Refractoring
    
    Co-authored-by: Niklas Pelz <pelznik@gmail.com>
    Co-authored-by: Jan Kaiser <jan.kaiser@devs-group.ch>

--- src/components/HomeCoffeeCards.vue
+++ src/components/HomeCoffeeCards.vue
@@ -22,25 +22,38 @@
 
                     <v-card-title primary-title>
                         <div>
+                            <v-layout row justify-end>
+                                <v-col cols="7">
+                                    <LanguageDropdown
+                                        v-model="selectedLanguage"
+                                        @input="changeLanguage(drink)"
+                                    ></LanguageDropdown>
+                                </v-col>
+                            </v-layout>
                             <h3 class="headline mb-0">{{ drink.title }}</h3>
                             <p></p>
-                            <div
-                                v-for="(paragraph,
-                                textIndex) in drink.paragraphs"
-                                :key="textIndex"
+                            <p
+                                :style="{
+                                    'background-color': diff
+                                        ? 'gold'
+                                        : 'initial'
+                                }"
+                                id="card-text"
                             >
-                                <p>{{ paragraph.text }}</p>
-                                <p></p>
-                            </div>
-                            <!--
-                            This can be used to insert empty lines into hero cards: (just empty paragraph)
+                                {{ drink.paragraph }}
+                            </p>
                             <p></p>
-                            <div>
-                              {{ drink.text }}
-                            </div>
-                            -->
                         </div>
                     </v-card-title>
+                    <v-flex class="text-right">
+                        <v-btn
+                            outlined
+                            color="red"
+                            id="regenerate-btn"
+                            v-on:click="regenerateText(drink)"
+                            >Regenerate</v-btn
+                        >
+                    </v-flex>
                 </v-card>
             </v-flex>
         </v-layout>
@@ -51,107 +64,108 @@
 import image1 from '../assets/cafe1_vert.jpg';
 import image2 from '../assets/cafe5_vert.jpg';
 import image3 from '../assets/cafe2_vert.jpg';
-import textFile1 from 'raw-loader!../ax_results/coffee_result1.txt';
-import textFile2 from 'raw-loader!../ax_results/coffee_result2.txt';
-import textFile3 from 'raw-loader!../ax_results/coffee_result3.txt';
+import textFileEspressoEN from 'raw-loader!../ax_results/espresso_EN.txt';
+import textFileLatteEN from 'raw-loader!../ax_results/latte_EN.txt';
+import textFileCappuccinoEN from 'raw-loader!../ax_results/cappuccino_EN.txt';
+import textFileEspressoDE from 'raw-loader!../ax_results/espresso_DE.txt';
+import textFileLatteDE from 'raw-loader!../ax_results/latte_DE.txt';
+import textFileCappuccinoDE from 'raw-loader!../ax_results/cappuccino_DE.txt';
+import LanguageDropdown from './LanguageDropdown';
+
+// eslint-disable-next-line no-unused-vars
+const sample = require('lodash.sample');
 
 export default {
-    name: 'HomePlans',
+    name: 'HomeCoffeeCards',
+    components: { LanguageDropdown },
     data() {
         return {
             drinks: [
                 {
                     title: 'EXAMPLE 1',
-                    paragraphs: [
-                        {
-                            text: 'Lorem ipsum'
-                        },
-                        {
-                            text: 'Dolor sit Amet'
-                        },
-                        {
-                            text: 'Consect Etur Adipiscing Elit'
-                        }
-                    ],
+                    paragraph: 'lorem ipsum',
                     img_url: image1,
-                    textFile: textFile1
+                    textFile: textFileEspressoEN
                 },
                 {
                     title: 'EXAMPLE 2',
-                    paragraphs: [
-                        {
-                            text: 'Lorem ipsum'
-                        },
-                        {
-                            text: 'Dolor sit Amet'
-                        },
-                        {
-                            text: 'Consect Etur Adipiscing Elit'
-                        }
-                    ],
+                    paragraph: 'lorem ipsum',
                     img_url: image2,
-                    textFile: textFile2
+                    textFile: textFileLatteEN
                 },
                 {
                     title: 'EXAMPLE 3',
-                    paragraphs: [
-                        {
-                            text: 'Lorem ipsum'
-                        },
-                        {
-                            text: 'Dolor sit Amet'
-                        },
-                        {
-                            text: 'Consect Etur Adipiscing Elit'
-                        }
-                    ],
+                    paragraph: 'lorem ipsum',
                     img_url: image3,
-                    textFile: textFile3
+                    textFile: textFileCappuccinoEN
                 }
-            ]
+            ],
+            languageFiles: {
+                textFileCappuccinoEN: textFileCappuccinoEN,
+                textFileEspressoEN: textFileEspressoEN,
+                textFileLatteEN: textFileLatteEN,
+                textFileCappuccinoDE: textFileCappuccinoDE,
+                textFileEspressoDE: textFileEspressoDE,
+                textFileLatteDE: textFileLatteDE
+            },
+            diff: false,
+            selectedLanguage: 'English'
         };
     },
     // TODO: Switch to Vuex and use Mutations to change states and request data:
     mounted() {
-        this.parseAXResponse();
+        this.drinks.forEach(x => {
+            this.regenerateText(x);
+        });
     },
     methods: {
-        parseAXResponse() {
-            // TODO: Currently only removes special signs instead of converting them
-            var WINDOWS_1252 =
-                '\u0000\u0001\u0002\u0003\u0004\u0005\u0006\u0007\b\t\n\u000b\f\r\u000e\u000f\u0010\u0011\u0012\u0013\u0014\u0015\u0016\u0017\u0018\u0019\u001a\u001b\u001c\u001d\u001e\u001f !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~€�‚ƒ„…†‡ˆ‰Š‹Œ�Ž��‘’“”•–—˜™š›œ�žŸ ¡¢£¤¥¦§¨©ª«¬­®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿ';
-
-            for (var i = 0; i < this.drinks.length; i++) {
-                var text = '';
-                var textFile = this.drinks[i].textFile;
-                for (var j = 0; j < textFile.length; j++) {
-                    text += WINDOWS_1252.charAt(textFile.charCodeAt(j));
-                }
-
-                let split = text.split('</h1>');
-                let coffee_title = split[0].split('<h1>')[1];
-                let coffee_paragraph_split = split[1].split('<p>');
-                let paragraph1 = coffee_paragraph_split[1].replaceAll(
-                    '</p>',
-                    ''
+        regenerateText(drink) {
+            const drinkIndex = this.drinks.findIndex(
+                dataDrink => dataDrink.title == drink.title
+            );
+            const textFile = this.drinks[drinkIndex].textFile;
+            let split = textFile.split('</h1>');
+            let coffee_title = split[0].split('<h1>')[1];
+            let coffee_paragraph_split = split[1].split('<p>');
+            let paragraphArray = [];
+            for (let j = 1; j < coffee_paragraph_split.length; j++) {
+                paragraphArray.push(
+                    coffee_paragraph_split[j].replaceAll('</p>', '')
                 );
-                let paragraph2 = coffee_paragraph_split[2].replaceAll(
-                    '</p>',
-                    ''
-                );
-                let paragraph3 = coffee_paragraph_split[3].replaceAll(
-                    '</p>',
-                    ''
-                );
-
-                this.drinks[i].title = coffee_title;
-                this.drinks[i].paragraphs[0].text = paragraph1;
-                this.drinks[i].paragraphs[1].text = paragraph2;
-                this.drinks[i].paragraphs[2].text = paragraph3;
             }
+
+            this.drinks[drinkIndex].title = coffee_title;
+            this.drinks[drinkIndex].paragraph = sample(paragraphArray);
+        },
+        changeLanguage(drink) {
+            switch (this.selectedLanguage) {
+                case 'English':
+                    this.assignTextFileString(drink, 'EN');
+                    break;
+                case 'German':
+                    this.assignTextFileString(drink, 'DE');
+                    break;
+            }
+            this.regenerateText(drink);
+        },
+        assignTextFileString(drink, languageCode) {
+            const drinkIndex = this.drinks.findIndex(
+                dataDrink => dataDrink.title == drink.title
+            );
+            let textFileString;
+            textFileString =
+                'textFile' + drink.title.split(' ')[0] + languageCode;
+            this.drinks[drinkIndex].textFile = eval(
+                'this.languageFiles.' + textFileString
+            );
+            console.log(this.drinks[drinkIndex].textFile);
         }
     }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+#card-text {
+    word-break: normal;
+}
+</style>
---
+++ src/components/LanguageDropdown.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-select
+        v-model="selectedLanguage"
+        :items="languages"
+        @input="changeLanguage()"
+        prepend-icon="mdi-translate"
+    ></v-select>
+</template>
+
+<script>
+export default {
+    name: 'LanguageDropdown',
+    data() {
+        return {
+            selectedLanguage: 'English',
+            languages: ['English', 'German']
+        };
+    },
+    methods: {
+        changeLanguage() {
+            this.$emit('input', this.selectedLanguage);
+        }
+    }
+};
+</script>
+
+<style scoped></style>
